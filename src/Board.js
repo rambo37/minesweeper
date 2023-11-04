a873572742982af4f38845446431ec1e3afb762e
Savraj <savrajbassi@gmail.com>
2023-11-04 18:25:16 +0000

    Improve css on narrow viewports

--- src/Board.js
+++ src/Board.js
@@ -1,17 +1,39 @@
+import React, { useRef, useEffect, useState } from "react";
 import { Square } from "./Square";
 
 export function Board({
-	cols,
+  cols,
   handleLeftClick,
   handleRightClick,
   squares,
   explodedSquareIndex,
   squareSize,
 }) {
+  const boardRef = useRef(null);
+  const [overflowing, setOverflowing] = useState(false);
+
+  useEffect(() => {
+    updateOverflowing();
+
+    window.addEventListener("resize", () => {
+      updateOverflowing();
+    });
+  }, [cols, squareSize]);
+
+  // Updates the overflowing state based on whether the board's scollWidth is
+  // greater than the board's offsetWidth (i.e., it is overflowing)
+  function updateOverflowing() {
+    setOverflowing(boardRef.current.scrollWidth > boardRef.current.offsetWidth);
+  }
+
   return (
     <div
+      ref={boardRef}
       className="board"
-      style={{ gridTemplateColumns: `repeat(${cols}, max-content)` }}
+      style={{
+        gridTemplateColumns: `repeat(${cols}, max-content)`,
+        justifyContent: `${overflowing ? "flex-start" : "center"}`,
+      }}
     >
       {squares.map((value, index) => {
         return (
